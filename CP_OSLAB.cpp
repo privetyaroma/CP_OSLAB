3d78cff7f47d71059cfbdcadc522089281fcedf5
Roman Van der Flaas <romanvanderflaas@gmail.com>
2022-11-07 08:48:31 +0500

    LAB 1.0

--- CP_OSLAB.cpp
+++ CP_OSLAB.cpp
@@ -7,6 +7,10 @@
  *
  */
 
+ //LAB1
+
+#include <fstream>
+#include <string>
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,6 +19,7 @@
 #include <locale.h>
 #include <time.h>
 #include <windowsx.h>
+
 using namespace std;
 
 #define KEY_SHIFTED     0x8000
@@ -30,8 +35,8 @@ const TCHAR szWinClass[] = _T("Win32SampleApp");
 const TCHAR szWinName[] = _T("Крестики-нолики");
 HWND hwnd;               /* This is the handle for our window */
 HBRUSH hBrush;           /* Current brush */
-int n = 3, r = 255, g = 0, b = 0, w, h;
-
+int n, width, height, R, G, B, r, g, b, w, h;
+int** m;
 /* Runs Notepad */
 
 void RunNotepad(void)
@@ -41,7 +46,7 @@ void RunNotepad(void)
 
     ZeroMemory(&sInfo, sizeof(STARTUPINFO));
 
-    puts("Starting Notepad...");
+    puts("Запуск блокнота...");
     CreateProcess(_T("C:\\Windows\\Notepad.exe"),
         NULL, NULL, NULL, FALSE, 0, NULL, NULL, &sInfo, &pInfo);
 }
@@ -69,8 +74,61 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
             LineTo(hdc, w * i, reClient.bottom);
             MoveToEx(hdc, reClient.left, h * i, NULL);
             LineTo(hdc, reClient.right, h * i);
+            DeleteObject(hPen);
 
         }
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+            {
+                if (m[i][j] == 1)
+                {
+                    SelectObject(hdc, GetStockObject(NULL_BRUSH));
+                    hPen = CreatePen(0, 6, RGB(0, 0, 128));
+                    SelectObject(hdc, hPen);
+                    if (w <= h)
+                    {
+                        Ellipse(hdc, (w * i) + 0.1 * w, (h * j + (h - w) / 2) + 0.1 * w, (w * (i + 1)) - 0.1 * w, (h * (j + 1) - (h - w) / 2) - 0.1 * w);
+                    }
+                    else
+                    {
+                        Ellipse(hdc, (w * i + (w - h) / 2) + 0.1 * h, (h * j) + 0.1 * h, (w * (i + 1) - (w - h) / 2) - 0.1 * h, (h * (j + 1)) - 0.1 * h);
+                    }
+                    DeleteObject(hPen);
+                }
+                else
+                {
+                    if (m[i][j] == 2)
+                    {
+                        SelectObject(hdc, GetStockObject(NULL_BRUSH));
+                        hPen = CreatePen(0, 6, RGB(0, 128, 0));
+                        SelectObject(hdc, hPen);
+                        if (w <= h)
+                        {
+                            MoveToEx(hdc, (w * i) + 0.1 * w, (h * j + (h - w) / 2) + 0.1 * w, NULL);
+                            LineTo(hdc, (w * (i + 1)) - 0.1 * w, (h * (j + 1) - (h - w) / 2) - 0.1 * w);
+                            MoveToEx(hdc, (w * (i + 1)) - 0.1 * w, (h * j + (h - w) / 2) + 0.1 * w, NULL);
+                            LineTo(hdc, (w * i) + 0.1 * w, (h * (j + 1) - (h - w) / 2) - 0.1 * w);
+                        }
+                        else
+                        {
+                            MoveToEx(hdc, (w * i + (w - h) / 2) + 0.1 * h, (h * j) + 0.1 * h, NULL);
+                            LineTo(hdc, (w * (i + 1) - (w - h) / 2) - 0.1 * h, (h * (j + 1)) - 0.1 * h);
+                            MoveToEx(hdc, (w * i + (w - h) / 2) + 0.1 * h, h * (j + 1) - 0.1 * h, NULL);
+                            LineTo(hdc, (w * (i + 1) - (w - h) / 2) - 0.1 * h, (h * j) + 0.1 * h);
+                        }
+                        DeleteObject(hPen);
+                    }
+                }
+            }
+
+
+
+
+
+
+        GetWindowRect(hwnd, &reClient);
+        width = reClient.right - reClient.left;
+        height = reClient.bottom - reClient.top;
         EndPaint(hwnd, &ps);
         DeleteObject(hPen);
     }
@@ -78,10 +136,17 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
     case WM_LBUTTONUP:
     {
 
-        int j = GET_Y_LPARAM(lParam) / h + 1;
-        int i = GET_X_LPARAM(lParam) / w + 1;
-        cout << j << " " << i << endl;
-        break;
+        if (m[GET_X_LPARAM(lParam) / w][GET_Y_LPARAM(lParam) / h] == 0)
+            m[GET_X_LPARAM(lParam) / w][GET_Y_LPARAM(lParam) / h] = 1;
+        InvalidateRect(hwnd, NULL, TRUE);
+        return 0;
+    }
+    case WM_RBUTTONUP:
+    {
+        if (m[GET_X_LPARAM(lParam) / w][GET_Y_LPARAM(lParam) / h] == 0)
+            m[GET_X_LPARAM(lParam) / w][GET_Y_LPARAM(lParam) / h] = 2;
+        InvalidateRect(hwnd, NULL, TRUE);
+        return 0;
     }
     case WM_MOUSEWHEEL:
     {    if ((short)HIWORD(wParam) < 0)
@@ -125,7 +190,10 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
         case KEY_ENTER:
         {
             DeleteObject(hBrush);
-            hBrush = CreateSolidBrush(RGB(rand() % 255, rand() % 255, rand() % 255));
+            R = rand() % 255;
+            G = rand() % 255;
+            B = rand() % 255;
+            hBrush = CreateSolidBrush(RGB(R, G, B));
             SetClassLongPtr(hwnd, GCLP_HBRBACKGROUND, (LONG)hBrush);
             InvalidateRect(hwnd, NULL, TRUE);
             break;
@@ -151,6 +219,57 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
 
 int main(int argc, char** argv)
 {
+    ifstream values;
+    values.open("values.txt");
+    if (!values.is_open())
+    {
+        if (argc < 2)
+            n = 3;
+        else
+            n = atoi(argv[1]);
+        width = 320;
+        height = 240;
+        R = 0;
+        G = 0;
+        B = 255;
+        r = 255;
+        g = 0;
+        b = 0;
+    }
+    else
+    {
+        string s;
+        getline(values, s);
+        if (argc < 2)
+            n = stoi(s);
+        else
+            n = atoi(argv[1]);
+        getline(values, s);
+        width = stoi(s);
+        getline(values, s);
+        height = stoi(s);
+        getline(values, s);
+        R = stoi(s);
+        getline(values, s);
+        G = stoi(s);
+        getline(values, s);
+        B = stoi(s);
+        getline(values, s);
+        r = stoi(s);
+        getline(values, s);
+        g = stoi(s);
+        getline(values, s);
+        b = stoi(s);
+        values.close();
+    }
+
+
+    m = new int* [n];
+    for (int i = 0; i < n; i++)
+        m[i] = new int[n];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            m[i][j] = 0;
     srand(time(NULL));
     setlocale(LC_ALL, "Rus");
     BOOL bMessageOk;
@@ -166,9 +285,10 @@ int main(int argc, char** argv)
     wincl.hInstance = hThisInstance;
     wincl.lpszClassName = szWinClass;
     wincl.lpfnWndProc = WindowProcedure;      /* This function is called by Windows */
+    wincl.hCursor = LoadCursor(NULL, IDC_ARROW); // class cursor 
 
     /* Use custom brush to paint the background of the window */
-    hBrush = CreateSolidBrush(RGB(0, 0, 255));
+    hBrush = CreateSolidBrush(RGB(R, G, B));
     wincl.hbrBackground = hBrush;
 
     /* Register the window class, and if it fails quit the program */
@@ -180,10 +300,10 @@ int main(int argc, char** argv)
         szWinClass,          /* Classname */
         szWinName,       /* Title Text */
         WS_OVERLAPPEDWINDOW, /* default window */
-        CW_USEDEFAULT,       /* Windows decides the position */
-        CW_USEDEFAULT,       /* where the window ends up on the screen */
-        320,                 /* The programs width */
-        240,                 /* and height in pixels */
+        300,       /* Windows decides the position */
+        300,       /* where the window ends up on the screen */
+        width,                 /* The programs width */
+        height,                 /* and height in pixels */
         HWND_DESKTOP,        /* The window is a child-window to desktop */
         NULL,                /* No menu */
         hThisInstance,       /* Program Instance handler */
@@ -209,13 +329,35 @@ int main(int argc, char** argv)
         /* Send message to WindowProcedure */
         DispatchMessage(&message);
     }
-
+    ofstream valuesout;
+    valuesout.open("values.txt");
+    valuesout << n;
+    valuesout << "\n";
+    valuesout << width;
+    valuesout << "\n";
+    valuesout << height;
+    valuesout << "\n";
+    valuesout << R;
+    valuesout << "\n";
+    valuesout << G;
+    valuesout << "\n";
+    valuesout << B;
+    valuesout << "\n";
+    valuesout << r;
+    valuesout << "\n";
+    valuesout << g;
+    valuesout << "\n";
+    valuesout << b;
+    valuesout << "\n";
     /* Cleanup stuff */
+    valuesout.close();
+    for (int i = 0; i < n; i++)
+        delete[] m[i];
+    delete[] m;
     DestroyWindow(hwnd);
     UnregisterClass(szWinClass, hThisInstance);
     DeleteObject(hBrush);
-
     //RunNotepad();
-
+    //cout << argv[0];
     return 0;
 }
